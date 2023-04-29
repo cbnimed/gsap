fc1ed4b4825d7da1208b3838f695ea2998f3173d
cbni media <130749004+cbnimed@users.noreply.github.com>
2023-04-29 08:38:22 +0700

    Add files via upload

---
+++ GSAPcdn.js
@@ -0,0 +1,8 @@
+{
+    "imports": {
+      "three" : "https://unpkg.com/three@0.126.1/build/three.module.js",
+      "orbitcontrols" : "https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js",
+      "datgui" : "https://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.7.9/dat.gui.min.js",
+      "gsap" : "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js"
+    }
+  }
